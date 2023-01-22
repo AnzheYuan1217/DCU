d03fa03987f07a1c25b8d8b810dbcc639e9e54e4
Anzhe Yuan <66779558+AnzheYuan1217@users.noreply.github.com>
2023-01-22 23:52:06 +0000

    Update 01busfares.py

--- ca278/week1/01busfares.py
+++ ca278/week1/01busfares.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-age = int(input())
-a = "Please enter your age:"
-if age >= 5 and age <= 16:
-    b = "Your fare is: 0.75 euro."
-elif age >= 17 and age <= 20:
-    b = "Your fare is: 1.25 euro."
-elif age >= 21 and age <= 64:
-    b = "Your fare is: 1.5 euro."
-else:
-    b = "You go free!"
+age = int(input("Please enter your age:"))
 
-print(a+b)
+if age >= 5 and age <= 16:
+    s = "Your fare is: 0.75 euro."
+elif age >= 17 and age <= 20:
+    s = "Your fare is: 1.25 euro."
+elif age >= 21 and age <= 64:
+    s = "Your fare is: 1.5 euro."
+else:
+    s = "You go free!"
+
+print(s)
