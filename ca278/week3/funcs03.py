c26fd89538316b93014a158feec2e96eb4337509
Anzhe Yuan <66779558+AnzheYuan1217@users.noreply.github.com>
2023-02-01 14:31:12 +0000

    week3 lab

---
+++ ca278/week3/funcs03.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env/ python3
+
+import time
+
+
+def get_price(age):
+    if age <= 16:
+        return 5
+    elif age > 60:
+        return 7
+    else:
+        return 10
+
+
+def weird_case(some_str):
+    up = True
+    weird_str = ""
+    for i in some_str:
+        if i.isalpha() and up:
+            weird_str += i.upper()
+            up = False
+        elif i.isalpha():
+            weird_str += i.lower()
+            up = True
+        else:
+            weird_str += i
+    return weird_str
+
+
+def every_second(l1, l2):
+    l3 = []
+    for i in range(1, len(l1), 2):
+        l3.append(l1[i])
+    for i in range(1, len(l2), 2):
+        l3.append(l2[i])
+    return l3
+
+
+def is_neg(num):
+    return num < 0
+
+
+def remove_neg(lst):
+    i = 0
+    while i < len(lst):
+        if is_neg(lst[i]):
+            lst.pop(i)
+            i -= 1
+        i += 1
+    return
+
+
+def countdown(num):
+    for i in range(num, 0, -1):
+        print(i)
+        time.sleep(0.1)
+    print("LIFT OFF!")
+    return
+
+
+def search(st, letter):
+    for i in st:
+        if letter == i:
+            return True
+    return False
+
+
+def index(s, letter):
+    return s.index(letter) if letter in s else -1
+
+
+def previous_two(n):
+    now = 1
+    pre = 0
+    seq = 0
+    for i in range(n):
+        seq = now + pre
+        now = pre
+        pre = seq
+    return seq
